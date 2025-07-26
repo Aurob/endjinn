288901d35eb4d5ef6a569bfe21ba897004a54b41
Rob <18496213+Aurob@users.noreply.github.com>
2025-07-25 21:36:04 -0400

    Delete endjinn directory

--- endjinn/shaders/text_fragment_es.glsl
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-varying vec2 vTexCoord;
-uniform sampler2D uTexture;
-uniform vec3 uColor;
-
-void main() {
-    vec4 texColor = texture2D(uTexture, vTexCoord);
-    gl_FragColor = vec4(uColor, texColor.a);
-}
